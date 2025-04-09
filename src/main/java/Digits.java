e61c8d755498196e9ecf4c9d0930f2954887b3c5
clovisZ <165089300+clovisZ@users.noreply.github.com>
2025-04-09 01:29:49 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,17 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    for (int i = 0; i < num.toString().length) {
+		digitList.add(0, num % 10);
+		num /= 10;
+	    }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		for (int i = 0; i < digitList.size()-1; i++)
+			if (digitList.get(i) >= digitList.get(i+1)) return false;
+		return true;
 	}
 	
 	public String toString()
