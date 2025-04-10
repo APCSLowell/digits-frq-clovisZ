4e3ae5cde04d1c8301962096bb3a0cabea0f9788
clovisZ <165089300+clovisZ@users.noreply.github.com>
2025-04-09 23:38:09 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,8 +6,9 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    for (int i = 0; i < (num + "").length(); i++) {
+	{ /* to be implemented in part (a) */
+	    int numLength = (num + "").length();
+	    for (int i = 0; i < numLength; i++) {
 		digitList.add(0, num % 10);
 		num /= 10;
 	    }
